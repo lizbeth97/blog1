8e2737d40282fd12d5af6fc079d8a1a52dd9d971
Liz <lizbeth.vazquez@cceo.com.mx>
2017-12-18 09:30:59 -0600

    Eloquent

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 //www.misitio.com =Rute:get('/');
 //www.misitio.com/contacto= Route:get('contacto', function(){});
 
-Route::get('/', ['as' => 'home', function(){
-    return view('home');
+//Route::get('/', ['as' => 'home', function(){
+//    return view('home');
+//
 
-//Route::get('/', ['as' => 'home', 'uses' => 'PagesControllers@home'])->middleware('example');
+Route::get('/', ['as' => 'home', 'uses' => 'PagesControllers@home']);
 
 
 Route::get('contactame', ['as' => 'contactos', 'uses' => 'PagesControllers@contact']);
@@ -14,24 +15,21 @@ Route::get('contactame', ['as' => 'contactos', 'uses' => 'PagesControllers@conta
 
 Route::get('saludos/{nombre?}',['as' => 'saludos', 'uses' => 'PagesControllers@saludo'])->where('nombre', "[A-Za-z]+");
 
-
 Route::post('contacto','PagesControllers@mensajes');
-
 
 Route::resource('mensajes', 'MessagesController');
 
-// Route::get('mensajes/create',['as'=>'messages.create', 'uses'=>'MessagesController@create']);
 
-// Route::post('mensajes',['as'=>'messages.store', 'uses'=>'MessagesController@store']);
+/*Route::get('mensajes/create',['as'=>'messages.create', 'uses'=>'MessagesController@create']);
 
-// Route::get('mensajes',['as'=>'messages.index', 'uses'=>'MessagesController@index']);
+Route::post('mensajes',['as'=>'messages.store', 'uses'=>'MessagesController@store']);
 
-// Route::get('mensajes/{id}',['as'=>'messages.show', 'uses'=>'MessagesController@show']);
+Route::get('mensajes',['as'=>'messages.index', 'uses'=>'MessagesController@index']);
 
-// Route::get('mensajes/{id}/edit',['as'=>'messages.edit', 'uses'=>'MessagesController@edit']);
+Route::get('mensajes/{id}',['as'=>'messages.show', 'uses'=>'MessagesController@show']);
 
-// Route::put('mensajes/{id}',['as'=>'messages.update', 'uses'=>'MessagesController@update']);
+Route::get('mensajes/{id}/edit',['as'=>'messages.edit', 'uses'=>'MessagesController@edit']);
 
-// Route::delete('mensajes/{id}',['as'=>'messages.destroy', 'uses'=>'MessagesController@destroy']);
+Route::put('mensajes/{id}',['as'=>'messages.update', 'uses'=>'MessagesController@update']);
 
-?>
+Route::delete('mensajes/{id}',['as'=>'messages.destroy', 'uses'=>'MessagesController@destroy']);*/
