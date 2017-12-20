442dcd891b4b635ecc929e2b2a92868a40580ad5
Liz <lizbeth.vazquez@cceo.com.mx>
2017-12-20 15:39:43 -0600

    Guardado modificaciones

--- resources/views/Mensajes/index.blade.php
+++ resources/views/Mensajes/index.blade.php
@@ -18,6 +18,21 @@
                     <td>{{$m->mensaje}}</td>
                 </tr>
             @endforeach
+
+            <td>
+                    <a class="btn btn-info btn-xs" 
+                    href="{{route('mensajes.edit', $message->id)}}">Editar</a>
+                    <form style="display: inline" 
+                        method="POST" 
+                        action="{{route('mensajes.destroy', $message->id)}}">
+                        {!!csrf_field()!!}
+                        {!! method_field('DELETE')!!}
+
+                        <button class="btn btn-danger btn-xs" type="submit">Eliminar</button>
+                        
+                    </form>
+                </td>
         </tbody>
+
 </table>
 @stop
